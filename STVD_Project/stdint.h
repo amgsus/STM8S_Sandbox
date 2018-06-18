2b0b46d9811be00d436673a30e89bf85a90fc5e1
AG <me@amgsus.net>
2018-06-18 21:03:28 +0300

    Add typedefs for signed types

--- STVD_Project/stdint.h
+++ STVD_Project/stdint.h
@@ -9,4 +9,8 @@ typedef char int8_t;
 typedef short int16_t;
 typedef long int32_t;
 
+typedef int8_t sint8_t;
+typedef int16_t sint16_t;
+typedef int32_t sint32_t;
+
 # endif // End of Include Guard //
